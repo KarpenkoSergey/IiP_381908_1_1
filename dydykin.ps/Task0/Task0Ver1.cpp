d853ceb40212d0310b842b24c0e03b335368d153
DydykinPS <61145813+DydykinPS@users.noreply.github.com>
2020-02-28 20:39:14 +0300

    Task0.Вариант0.Версия2

--- dydykin.ps/Task0/Task0Ver1.cpp
+++ dydykin.ps/Task0/Task0Ver1.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <clocale>
 using namespace std;
+void typeoftriangle(int x1, int x2, int x3)//функция для определения вида треугольника + проверка на его существование
+{
+	if ((x1 > x2 + x3) || (x2 > x1 + x3) || (x3 > x1 + x2))
+		cout << "Треугольник не существует";
+	else if (x1*x1==x2*x2+x3*x3)
+		cout << "Треугольник прямоугольный";
+		else if (x1 * x1 > x2 * x2 + x3 * x3)
+			cout << "Треугольник тупоугольный";
+			else cout << "Треугольник остроугольный";
+}
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	setlocale(LC_CTYPE, "Russian");
 	int a, b, c;//стороны треугольника
-	cin >> a >> b >> c;//считывание сторон треугольника
-	//воспользуемся правилом из геометрии для определения вида треугольника
-	if (c * c < a * a + b * b)
-		cout << "Треугольник остроугольный";
-	if (c * c > a * a + b * b)
-		cout << "Треугольник тупоугольный";
-	if (c * c == a * a + b * b)
-		cout << "Треугольник прямоугольный";
+	cout << "Необходимо ввести стороны треугольника в порядке убывания ";
+	cin >> a >> b >> c;
+	typeoftriangle(a, b, c);
 	return 0;
 }
