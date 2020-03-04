b703b2009d91907d7ee49ab4ea00f27bd5229dea
PyatkinNS <61143368+PyatkinNS@users.noreply.github.com>
2020-03-04 22:05:37 +0300

    Task1 Вариант 3 Версия 1

---
+++ pyatkin.ns/Task1/testmain.cpp
@@ -0,0 +1,34 @@
+#include "Money.h"
+
+int main() {
+	Money c1(1, 60000), c2, c3, c4(600, 200), c5, c6(10, 10), c7(1, 90), c8, c9;
+	c2 = c1;
+	c3 = c2 - c4;
+	c5 = c6 / 2;
+
+	cout << c1 << endl;
+	cout << c2 << endl;
+	cout << c3 << endl;
+	cout << c5 << endl;
+	if (c1 > c4)
+	{
+		cout << "don't show" << endl;
+	}
+	if (c1 < c4)
+	{
+		cout << "show" << endl;
+	}
+	else {
+		cout << "don't show" << endl;
+	}
+	if (c8 == c9) {
+
+		cout << "show" << endl;
+	}
+
+	Money* pc1 = new Money(c1);
+	delete pc1;
+
+
+	return 0;
+}
