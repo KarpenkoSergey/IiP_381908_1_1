2262cdb7f950755e95bca5e818b777eda183f440
GordeevVV <61159998+GordeevVV@users.noreply.github.com>
2020-03-09 13:58:32 +0300

    Update Vector3D.cpp

--- gordeev.vv/Task1/Vector3D.cpp
+++ gordeev.vv/Task1/Vector3D.cpp
@@ -2,21 +2,44 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+//Конструктор по умолчанию
 Vector3D ::Vector3D(void)
 {
 	x = 0; y = 0; z = 0;
 }
+//Конструктор преобразования типа
 Vector3D::Vector3D(double d)
 {
 	x = d;
 	y = 0;
 	z = 0;
 }
+//Конструктор инициализатор
+Vector3D::Vector3D(double _x, double _y, double _z)
+{
+	x = _x;
+	y = _y;
+	z = _z;
+}
+//Конструктор копирования
+Vector3D::Vector3D(const Vector3D& C)
+{
+	x = C.x;
+	y = C.y;
+	z = C.z;
+}
+//Деструктор
+Vector3D::~Vector3D()
+{
+	x = 0; y = 0; z = 0;
+}
+//Перегрузка оператора =
 Vector3D Vector3D :: operator=(const Vector3D& C)
 {
 	x = C.x; y = C.y; z = C.z;
 	return *this;
 }
+//Перегрузка оператора +
 Vector3D Vector3D ::operator+(const Vector3D& C)
 {
 	Vector3D res;
@@ -25,7 +48,7 @@ Vector3D Vector3D ::operator+(const Vector3D& C)
 	res.z = z + C.z;
 	return res;
 }
-
+//Перегрузка оператора -
 Vector3D Vector3D ::operator-(const Vector3D& C)
 {
 	Vector3D res;
@@ -34,14 +57,15 @@ Vector3D Vector3D ::operator-(const Vector3D& C)
 	res.z = z - C.z;
 	return res;
 }
-//Скалярное произведение векторов
+
+//Перегрузка операции скалярного произведения векторов
 double  Vector3D ::sclprvect(const Vector3D& C)
 {
 	double res;
 	res = x * C.x + y * C.y + z * C.z;
 	return res;
 }
-//
+//Перегрузка операции умножения вектора на число
 Vector3D Vector3D::umnnachislo(int m)
 {
 	Vector3D res;
@@ -50,6 +74,7 @@ Vector3D Vector3D::umnnachislo(int m)
 	res.z = z * m;
 	return res;
 }
+//Перегрузка операции сравнения двух векторов
 Vector3D Vector3D ::sravnenie(const Vector3D& C)
 {
 	if(sqrt(x*x+y*y+z*z)>sqrt(C.x*C.x+C.y*C.y+C.z*C.z))
@@ -58,15 +83,13 @@ Vector3D Vector3D ::sravnenie(const Vector3D& C)
 		cout << "2 вектор больше 1" << endl;
 	return C;
 }
+//Перегрузка оператора <<
 ostream& operator<<(ostream& stream, const Vector3D& C)
 {
-	stream << C.x << endl;
-
-	stream << C.y << endl;
-
-	stream << C.z << endl;
+	stream << C.x << " " << C.y << " " << C.z << " " << endl;
 	return stream;
 }
+//Перегрузка оператора >>
 istream& operator>> (istream& stream, Vector3D& C)
 {
 	stream >> C.x;
