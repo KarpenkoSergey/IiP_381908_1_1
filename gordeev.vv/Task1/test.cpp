3cc9a291f8ee9b59a9458bd104fd3d3ec2140fc4
GordeevVV <61159998+GordeevVV@users.noreply.github.com>
2020-03-09 13:59:40 +0300

    Update test.cpp

--- gordeev.vv/Task1/test.cpp
+++ gordeev.vv/Task1/test.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include<locale.h>
 #include <math.h>
+#include <fstream>
 using namespace std;
 int main()
 {
 	setlocale(LC_CTYPE, "Russian");
 	int k;
+	ofstream strout("file.txt");
 	Vector3D V1, V2,V3;
 	cout << "V1(x,<enter>,y,<enter>,z,<enter>)=" << endl;
 	cin >> V1;
@@ -15,11 +17,21 @@ int main()
 	cin >> V2;
 	cout << "V2=" << endl << V2 << endl;
 	V3 = V2;
+	strout << V1 << V2  << V3<< endl;	
+	strout.close();
+	ifstream strin("file.txt");
+	strin >> V1;
+	cout<< endl;
+	strin >> V2;
+	cout<< endl;
+	strin >> V3;
+	cout << endl;
+	strin.close();
 	cout << "Сумма векторов (V1+V3)" << endl;
 	cout << V1 + V3 << endl;
 	cout << "Разность векторов (V1-V2)" << endl;
 	cout << V1 - V2 << endl;
-	cout << "Произведение векторов(V1*V3)" << endl;
+	cout << "Скалярное произведение векторов(V1*V3)" << endl;
 	cout << V1.sclprvect( V3) << endl;
 	cout << "Введите целое число" << endl;
 	cin >> k;
@@ -27,6 +39,5 @@ int main()
 	cout << V1.umnnachislo(k) << endl;
 	cout << " Сравнение векторов (V1 and V3)" << endl;
 	cout << V1.sravnenie(V3) << endl;
-	return 0;
-		
+	return 0;		
 }
