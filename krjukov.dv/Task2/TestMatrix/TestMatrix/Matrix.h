9d3c86c079a64fd05f5d7c15e60f709f77383124
KrjukovDV <Den190401@yandex.ru>
2020-03-25 21:10:14 +0300

    Task2. Вариант 6 Версия 1

--- krjukov.dv/Task2/TestMatrix/TestMatrix/Matrix.h
+++ krjukov.dv/Task2/TestMatrix/TestMatrix/Matrix.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <iostream>
+enum class ExceptionType {sizeError, outOfBoundsMatrix};
+struct Exception
+{
+	ExceptionType error;
+	Exception(ExceptionType _error);
+};
 class Matrix
 {
 private:
@@ -13,14 +19,16 @@ public:
 	~Matrix();
 	Matrix& operator=(const Matrix& m);
 	Matrix operator+(const Matrix& m);
-	Matrix operator-(const Matrix& m);
 	Matrix operator*(const Matrix& m);
 	Matrix operator*(int num);
+	friend Matrix operator* (int num, Matrix& m);
 	int* operator[](int _i);
-	void transpose();
+	Matrix transpose();
 	bool diagonallyDominant();
 	friend std::ostream& operator<< (std::ostream& stream, const Matrix& m);
 	friend std::istream& operator>> (std::istream& stream, Matrix& m);
+	void create(int _size);
+	void del();
 };
 std::ostream& operator<< (std::ostream& stream, const Matrix& t);
 std::istream& operator>> (std::istream& stream, Matrix& t);
